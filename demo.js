b1fb5e993fda4fd82a2387b133c9a4a521748a4d
chiakicage <841851568@qq.com>
2020-10-22 14:38:10 +0800

    promise

--- demo.js
+++ demo.js
@@ -9,15 +9,28 @@ mocker.login('123', '123', function (data, error) {
         console.log(data);
         let arr = [];
         let cnt = data.length;
-        for (let id of data) {
-            mocker.item(token, id, function(data, error) {
-                if (error) throw error;
-                console.log(data);
-                arr.push(data);
-                if (arr.length === cnt) {
-                    console.log(arr);
-                }
+        let p = new Promise((resolve, reject) => {
+            if (data.length === 0) reject('Empty Data!');
+            mocker.item(token, data[0], resolve);
+        });
+        for (let i = 0; i < data.length; i++) {
+            p = p.then((res, error) => {
+                return new Promise((resolve, reject) => {
+                    if (error) reject(error);
+                    else {
+                        arr.push(res);
+                        if (i + 1 < data.length) {
+                            mocker.item(token, data[i + 1], resolve);
+                        } else resolve();
+                    }
+                })
             });
         }
+        p.then(() => {
+            console.log(arr);
+        }).catch((error) => {
+            throw error;
+        });
+        console.log('done.');
     })
 })
